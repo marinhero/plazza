2dc06ce8636c0dc1826f767303a837ca5b77564f
marinhero <marin.alcaraz@gmail.com>
2013-04-15 15:36:23 +0200

    crash

--- parser.cpp
+++ parser.cpp
@@ -5,6 +5,8 @@
 // Login   <alcara_m@epitech.net>
 //
 // Started on  Mon Apr 15 15:30:19 2013 Marin Alcaraz
-// Last update Mon Apr 15 15:30:28 2013 Marin Alcaraz
+// Last update Mon Apr 15 15:36:07 2013 Marin Alcaraz
 //
 
+OH NOOOOOOO!
+I'M CRASHING THE FILE
