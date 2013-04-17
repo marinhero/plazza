d3b7d1aad47dce2d58f8c95ec4ec1563a837a402
Marin Alcaraz <hero@Crunchy>
2013-04-17 16:17:47 +0100

    curses

---
+++ reception.hh
@@ -0,0 +1,25 @@
+//
+// reception.hh for plazza in /home/hero/Dropbox/EPITECH/II/C++/plazza
+//
+// Made by Marin Alcaraz
+// Login   <alcara_m@epitech.net>
+//
+// Started on  Wed Apr 17 15:24:50 2013 Marin Alcaraz
+// Last update Wed Apr 17 15:53:39 2013 Marin Alcaraz
+//
+
+#ifndef RECEPTION_HH
+# define RECEPTION_HH
+
+#include <iostream>
+#include <ncurses.h>
+
+class   Reception
+{
+    public:
+        Reception();
+        ~Reception();
+        void    display();
+};
+
+#endif /* RECEPTION_HH */
