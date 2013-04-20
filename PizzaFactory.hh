ebd8a1b915f5b658ddf0c1535796c9d54328dd8f
ivanignatiev <ignatiev.ivan@gmail.com>
2013-04-20 17:44:07 +0200

    add pizza factory and different pizzas types

---
+++ PizzaFactory.hh
@@ -0,0 +1,45 @@
+/*
+** PizzaFactory.h for plazza in /home/ignatiev/Projects/plazza
+**
+** Made by ivan ignatiev
+** Login   <ignati_i@epitech.net>
+**
+** Started on  Sat Apr 20 14:51:17 2013 ivan ignatiev
+** Last update Sat Apr 20 15:01:25 2013 ivan ignatiev
+*/
+
+#ifndef PIZZAFACTORY_H_
+# define PIZZAFACTORY_H_
+
+# include <map>
+# include <string>
+
+# include "Debug.hh"
+# include "Pizza.hh"
+
+class PizzaFactory
+{
+    private:
+        static int factory_count_;
+        static std::map<std::string, Pizza *>                pizzas_;
+        static std::map<std::string, Pizza::TaillePizza>    sizes_;
+        static ingr_stlist_t                                ingrs_;
+
+        PizzaFactory(PizzaFactory const &);
+        PizzaFactory &operator=(PizzaFactory const &);
+    public:
+        PizzaFactory(void);
+
+        static void learn(void);
+
+        static void learnSize(std::string const &size, Pizza::TaillePizza);
+        static void learnPizza(Pizza &);
+        static Pizza &createPizza(std::string const &type, std::string const &size);
+        static ingr_stlist_t const &ingrList(void);
+
+        static void forget(void);
+
+        ~PizzaFactory(void);
+};
+
+#endif /* !PIZZAFACTORY_H_ */
--- tests/PizzaTest.cpp
+++ tests/PizzaTest.cpp
@@ -5,15 +5,28 @@
 // Login   <ignati_i@epitech.net>
 //
 // Started on  Tue Apr 16 15:13:50 2013 ivan ignatiev
-// Last update Tue Apr 16 15:16:33 2013 ivan ignatiev
+// Last update Sat Apr 20 17:42:20 2013 ivan ignatiev
 //
 
-#include "Pizza.hh"
+#include "PizzaFactory.hh"
 #include "tests/test.h"
+
+static  PizzaFactory    pizza_factory;
 
 int     main(int argc, char **argv)
 {
-    Pizza   pizza(Pizza::Margarita, Pizza::XXL);
-    printf("Pizza [ size : %d type : %d ] \n", pizza.getSize(), pizza.getType());
+    Pizza &pizza1 = PizzaFactory::createPizza("margarita", "XXL");
+    std::cout << "type : " << pizza1.getStrType() << " " <<  pizza1.getType() <<  "  size : " << pizza1.getStrSize()  << " " << pizza1.getSize() << std::endl;
+
+    Pizza &pizza2 = PizzaFactory::createPizza("americana", "XL");
+    std::cout << "type : " << pizza2.getStrType() << " " <<  pizza2.getType() <<  "  size : " << pizza2.getStrSize()  << " " << pizza2.getSize() << std::endl;
+
+    Pizza &pizza3 = PizzaFactory::createPizza("regina", "L");
+    std::cout << "type : " << pizza3.getStrType() << " " <<  pizza3.getType() <<  "  size : " << pizza3.getStrSize()  << " " << pizza3.getSize() << std::endl;
+
+    Pizza &pizza4 = PizzaFactory::createPizza("fantasia", "M");
+    std::cout << "type : " << pizza4.getStrType() << " " <<  pizza4.getType() <<  "  size : " << pizza4.getStrSize()  << " " << pizza4.getSize() << std::endl;
+
+
     return (EXIT_SUCCESS);
 }
