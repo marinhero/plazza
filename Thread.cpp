5681461e55ce30a33dba9e5f6b1b9de45b922b20
ivanignatiev <ignatiev.ivan@gmail.com>
2013-04-21 13:46:36 +0200

    add thread class and kitchen stock

---
+++ Thread.cpp
@@ -0,0 +1,41 @@
+//
+// Thread.cpp for plazza in /home/ignatiev/Projects/plazza
+//
+// Made by ivan ignatiev
+// Login   <ignati_i@epitech.net>
+//
+// Started on  Wed Apr 17 21:09:46 2013 ivan ignatiev
+// Last update Sun Apr 21 13:40:32 2013 ivan ignatiev
+//
+
+#include "Thread.hh"
+
+Thread &Thread::operator=(Thread const &)
+{
+    return (*this);
+}
+
+Thread::Thread(IThreaded &threadobj)
+    : threadobj_(threadobj)
+{
+    if (pthread_create(&this->thread_, NULL, Thread::proccess, reinterpret_cast<void*>(this)) != 0)
+        throw new std::exception();
+}
+
+Thread::~Thread(void)
+{
+    pthread_join(this->thread_, NULL);
+}
+
+void    *Thread::run(void)
+{
+    return (this->threadobj_.run());
+}
+
+void    *Thread::proccess(void *obj)
+{
+    Thread  *th = reinterpret_cast<Thread *>(obj);
+    return (th->run());
+}
+
+
